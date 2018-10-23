ceb15d5aa6bf346fe29a383497c30b9d50af07d7
ayates58 <ajyates@Unomaha.edu>
2018-10-23 01:09:06 -0500

    Updated the Order forms

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
-from .tasks import order_created
-
 
 def order_create(request):
     cart = Cart(request)
@@ -13,13 +11,11 @@ def order_create(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
-                                        product=item['product'],
-                                        price=item['price'],
-                                        quantity=item['quantity'])
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
             # clear the cart
             cart.clear()
-            # launch asynchronous task
-            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
